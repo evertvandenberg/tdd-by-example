353e60a510f84c0bd8fe96ced7fed8bdc3bf136b
Evert van den Berg <evert.van.den.berg@ing.com>
2023-09-29 11:23:24 +0200

    No direct references to Franc and Dollar classes within test, using the Money static methods dollar and franc to iniate an object

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,9 +1,17 @@
 package guru.springframework;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
+    public abstract Money times (int multiplier);
 
+    public static Money dollar (int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc (int amount){
+        return new Franc(amount);
+    }
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return this.amount == money.amount
