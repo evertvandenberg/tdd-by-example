a54a0190213562c4641490fefe72d686ef876a02
Evert van den Berg <evert.van.den.berg@ing.com>
2023-09-29 13:46:36 +0200

    Added new class Bank and Expression

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -26,7 +26,15 @@ public class MoneyTest {
         assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
+    @Test
+    void testSimpleAddition() {
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
 
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
+    }
 
     @Test
     void testCurrency() {
